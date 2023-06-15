31268d904d023acd98e8c0ff60641eb1dc524027
Johnmichael Nichols <46973377+johnmichaeldnichols@users.noreply.github.com>
2023-06-15 16:09:12 -0400

    feat/working on gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,13 +1,31 @@
-import React from "react";
+// import React, { useReducer } from "react";
 import './gallery.css';
 import img1 from "../images/about/img1.jpg";
 import img2 from "../images/about/img2.jpg";
 import img3 from "../images/about/img3.jpg";
 import img4 from "../images/about/img4.jpg";
 
+// import React, { Component } from 'react';
+// import Carousel from '@brainhubeu/react-carousel';
+// import '@brainhubeu/react-carousel/lib/style.css';
+
+
 const Gallery = () => {
 
     const images = [img1,img2,img3,img4];
+
+// export default class Gallery extends Component {
+//   render() {
+//     return (
+//       <Carousel>
+//         <img src={img1} />
+//         <img src={img2} />
+//         <img src={img3} />
+//         <img src={img4} />
+//       </Carousel>
+//     );
+//   }
+// };
 
     return (
         <div className="gallery">
