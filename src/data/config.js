6e373b8047555dbffcf3dd4369efef2cb53800f8
Johnmichael Nichols <46973377+johnmichaeldnichols@users.noreply.github.com>
2023-06-24 21:06:53 -0400

    fix/location fix

--- src/data/config.js
+++ src/data/config.js
@@ -1,7 +1,7 @@
 const config = {
     email: "greenhousespawr@gmail.com",
     phoneNumber: "478-300-2070",
-    location: "342 Corder Rd",
+    location: "342 Corder Rd Suite A",
     address: "Warner Robins, GA 31088",
     mapsUrl:"https://goo.gl/maps/nDvJsG6t9kDeL1Re7",
     hours: {
